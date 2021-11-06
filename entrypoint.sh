aaf5fb3b84e70707b40b5c92320d01ba755119d5
Péter <peter@perenyi.net>
2021-11-06 15:05:40 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,28 @@
 
 
 # TODO: 1) check if first time run 2) check if backup exists -> restore 3) no backup -> run wizard
-# first time will run the wizard on port 5015
-echo 1 | /usr/sbin/3CXWizard
+
+. /usr/share/3cxpbx/3cxpbx.conf
+
+# first run?
+if [ ! -d $var ]; then
+   echo Configure instance ...
+   
+   # has setup config
+   if [ -f /etc/3cxpbx/setupconfig.xml ]; then
+     echo Using existing /etc/3cxpbx/setupconfig.xml   
+   else   
+     # has backup?
+     if [ -f $3CX_BACKUP_FILE ]; then
+       echo Restore from $3CX_BACKUP_FILE ...
+       echo Generate default restore config /etc/3cxpbx/setupconfig.xml ...
+     else
+       echo Run WebConfig on port :5015
+     fi   
+   fi  
+   
+   # run wizard
+   echo 1 | /usr/sbin/3CXWizard
+fi
 
 exec /lib/systemd/systemd --log-target=console --log-level=err
