6c539e5c2e52e3986ad757500381752bb13fb01f
Péter <peter@perenyi.net>
2022-02-15 12:55:20 +1100

    Update test-3cx.sh

--- test-3cx.sh
+++ test-3cx.sh
@@ -2,6 +2,9 @@
 
 echo Testing...
 
+echo Sample restore config:
+envsubst < /usr/local/share/setupconfig-3cx-restore.xml
+
 echo -n Wait for webconfig
 while ! netstat -tna | grep 'LISTEN\>' | grep -q ':5015\>'; do
   echo -n .
