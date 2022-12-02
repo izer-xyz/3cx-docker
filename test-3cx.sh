4648ff043ce3ecbe33f5eea9d2ecc9b211f5b2ec
Péter <peter@perenyi.net>
2022-12-02 11:25:52 +1100

    Update test-3cx.sh

--- test-3cx.sh
+++ test-3cx.sh
@@ -4,7 +4,7 @@ echo Sample restore config:
 envsubst < /usr/local/share/setupconfig-3cx-restore.xml
 
 echo -n Wait for webconfig
-if ! curl --retry 100  --retry-delay 2 --retry-all-errors localhost:5015; do
+if ! curl --retry 100  --retry-delay 2 --retry-all-errors localhost:5015; then
   echo Failed.
   exit -1
 done
